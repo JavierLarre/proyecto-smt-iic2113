47638faec811ae1203d567fda2dc2032e4dfc244
Javier Larre <javier.larre@uc.cl>
2025-05-31 13:06:39 -0400

    Added View Decorator

---
+++ Shin-Megami-Tensei-View/Views/ViewDecorator.cs
@@ -0,0 +1,19 @@
+﻿namespace Shin_Megami_Tensei_View.Views;
+
+public class ViewDecorator: IView
+{
+    private IView _previous;
+    private IView _new;
+
+    public ViewDecorator(IView newView, IView previousView)
+    {
+        _previous = previousView;
+        _new = newView;
+    }
+
+    public void Display()
+    {
+        _previous.Display();
+        _new.Display();
+    }
+}
